5bf0fbca3e56fc7c30cdb43ec42e6f499ef75ad5
VeroKoles <97703518+verokoles@users.noreply.github.com>
2022-04-17 01:15:09 -0500

    readme added

---
+++ README.md
@@ -0,0 +1,82 @@
+# Team Profile Generator::arrow_down: 
+  [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+ 
+  
+  > ## generate a list of employees on a team using Node in the command line
+     - run tests for employees, engineers, interns, and manager(s).
+     - an HTML is generated with IDs of all employees, along with names, roles, and/or email, Github usernames, schools, office numbers.
+
+
+
+   ## Table of Contents::mag:
+   1. [ Installation ](#installation)
+   2. [ Usage ](#usage)
+   3. [ License ](#license)
+   4. [ Contributing ](#contributing)
+   5. [ Tests ](#tests)
+
+   ## Installation:
+
+   To install dependancies, run the following commands:
+
+     ```
+     npm install
+     ```
+     ```
+     npm i express
+     ```
+     ```
+     npm i inquirer
+     ```
+
+
+   ## Usage:
+    
+    For usage, please make sure to add comments and submit merge requests to Github.com.
+
+
+   ## License: 
+
+     Apache-2.0 
+
+     [Link to Apache-2.0 license](https://opensource.org/licenses/Apache-2.0)
+
+
+  
+
+   ## Contributing:
+   Please be kind and professional when adding to or accessing this repository. Thank you!
+  [More on Contribution Guidelines](https://github.com/verokoles/readme-generator/blob/f57cf6a98bf276960885496059df4b039247c985/contributing.md)
+  
+   ## Tests:
+
+     In order to run tests, use command:
+
+   ```
+   npm run test Employee.test.js
+   ```
+   ```
+   npm run test Engineer.test.js
+   ```
+   ```
+   npm run test Intern.test.js
+   ```
+   ```
+   npm run test Manager.test.js
+   ```
+As shown below, all tests can be run together using 'npm run test':
+
+ ![Tests ran and passed](tests.png) 
+
+   ## Questions?:raising_hand:
+   
+
+  ### Reach out via:
+
+     Github: verokoles
+
+     email: verokoles88@gmail.com
+
+
+
+  [link to README tutorial video](https://drive.google.com/file/d/1s66xWH3goYwT6oh8B_IP8e2ty5ZHu6ap/view)
--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const generateHTML = require('./src/html-template');
 
+const DIST_DIR = path.resolve(__dirname);
+const distPath = path.join(DIST__DIR, 'index.html');
+const render = require('./src/html-template.js')
+
 const employees = [];
 // create questions for manager, adding engineer or intern, 
 //run all question, ush info to employees[]
@@ -21,7 +25,7 @@ function managerQuestions() {
         {
             type: 'input',
             name: 'managerId',
-            message: "What is their ID?",
+            message: "What is the manager ID?",
 
         },
         {
@@ -29,27 +33,84 @@ function managerQuestions() {
             name: 'managerEmail',
             message: "What is the email of the manager?",
 
+        },
+        {
+            type: 'input',
+            name: 'managerOfficeNumber',
+            message: "What is the office number of the manager?",
+
         }
     ]).then((data) => {
-        const manager = new Manager(data.managerName, data.maangerId, data.manager)
+        const manager = new Manager(data.managerName, data.managerId, data.managerEmail, managerOfficeNumber)
         employees.push(manager);
 
     })
 
 
 }
-// function engineerQuestions() {
-//     inquirer.prompt([
-//         {}
-//     ])
-// }
-// function internQuestions() {
-//     inquirer.prompt([
-//         {
+function engineerQuestions() {
+    inquirer.prompt([
 
-//         }
+        {
+            type: 'input',
+            name: 'engineerName',
+            message: "What is the name of the engineer?",
+
+        },
+        {
+            type: 'input',
+            name: 'engineerId',
+            message: "What is the Id of the engineer?",
+
+        },
+        {
+            type: 'input',
+            name: 'engineerGithub',
+            message: "What is the Github username of the engineer?",
+
+        },
+        {
+            type: 'input',
+            name: 'engineerEmail',
+            message: "What is the email of the engineer?",
+
+        }
+}
+ ]).then((data) => {
+    const engineer = new Engineer(data.engineerName, data.engineerId, data.engineerEmail, engineerGithub)
+    employees.push(engineer);
+
+})
 //     ])
 // }
+function internQuestions() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'internName',
+            message: "What is the name of the intern?",
+
+        },
+        {
+            type: 'input',
+            name: 'internId',
+            message: "What is the ID of the intern?",
+
+        },
+        {
+            type: 'input',
+            name: 'internEmail',
+            message: "What is the email of the intern?",
+
+        },
+        {
+            type: 'input',
+            name: 'internSchool',
+            message: "What is the school the intern attended?",
+
+        }
+        }
+    ])
 function createTeam() {
     inquirer.prompt([
         {
@@ -72,5 +133,5 @@ function createTeam() {
 }
 function builtTeam() {
     fs.writeFileSync()
-generateHTML(employees)
+    generateHTML(employees)
 }
