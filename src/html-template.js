37a6e70ea3a0dc165622925ba00ca510df8f115f
VeroKoles <97703518+verokoles@users.noreply.github.com>
2022-04-19 17:44:09 -0500

    html

--- src/html-template.js
+++ src/html-template.js
@@ -24,6 +24,19 @@ const generateTeam = team => {
     // html portion for engineer(s)
     const generateEngineer = engineer => {
         return `
+        <!DOCTYPE html>
+        <html lang="en">
+        
+        <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+            <title>My Team</title>
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+                integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+            <link rel="stylesheet" href="style.css">
+            <script src="https://kit.fontawesome.com/c502137733.js"></script>
+        </head>
     <div class="card employee-card">
 <div class="card-header">
 <h2 class="card-title">${engineer.getName()}</h2>
